650ba562762f4d5f84305205284c8878208115d9
Jaeyong <study@snu.ac.kr>
2021-11-29 02:19:51 +0900

    fixed word buttons

--- frontend/src/main.js
+++ frontend/src/main.js
@@ -1,78 +1,71 @@
-const json_url = "https://raw.githubusercontent.com/dlacksthd94/DAV-project/main/backend/data/graph_epi.json"
-const csv_url = "https://raw.githubusercontent.com/dlacksthd94/DAV-project/main/backend/data/Aesop.csv"
+const json_url = "https://raw.githubusercontent.com/dlacksthd94/DAV-project/main/backend/data/graph_epi.json";
+const csv_url = "https://raw.githubusercontent.com/dlacksthd94/DAV-project/main/backend/data/Aesop.csv";
 
-function renderButtons() {
-    // create buttons of words
+function renderButtons(){
     $.getJSON(json_url, function (json) {
-        var container = document.getElementById('buttons')
+        var container = document.getElementById('buttons');
         for (var i = 0; i < json.nodes.length; i++) {
-            var obj = json.nodes[i]
-            var html = '<button type="button" class="btn btn-primary m-1" data-bs-toggle="button" autocomplete="off">' + obj.name + '</button>'
-            container.innerHTML += html
-        }
-    })
+            var obj = json.nodes[i];
+            var html = '<button type="button" class="btn btn-primary m-1" data-bs-toggle="button" autocomplete="off">' + obj.name + '</button>';
+            container.innerHTML += html;
+        };
+    });
 };
-
 function addAccordion(container, num, title, episode) {
-    var html = '<div class="accordion-item"><h2 class="accordion-header" id="heading' + num + '"><button class="accordion-button collapsed" type="button" data-bs-toggle="collapse" data-bs-target="#collapse' + num + '" aria-expanded="true" aria-controls="collapse' + num + '">' + title + '</button></h2><div id="collapse' + num + '" class="accordion-collapse collapse" aria-labelledby="heading' + num + '" data-bs-parent="#accordionGroup"><div class="accordion-body">' + episode + '</div></div></div>'
-    container.innerHTML += html
-};
-
-
+    var html = '<div class="accordion-item"><h2 class="accordion-header" id="heading' + num + '"><button class="accordion-button collapsed" type="button" data-bs-toggle="collapse" data-bs-target="#collapse' + num + '" aria-expanded="true" aria-controls="collapse' + num + '">' + title + '</button></h2><div id="collapse' + num + '" class="accordion-collapse collapse" aria-labelledby="heading' + num + '" data-bs-parent="#accordionGroup"><div class="accordion-body">' + episode + '</div></div></div>';
+    container.innerHTML += html;
+}
 function renderStories() {
     d3.csv(csv_url, function (csv) {
-        titles = [...new Set(csv.map(d => d.title))];
-        episodes = [...new Set(csv.map(d => d.episode))];
-
-        // console.log(titles.length)
-        var container = document.getElementById("accordionGroup")
-        container.innerHTML = ""
+        titles = [...new Set(csv.map(d => d.title))]
+        episodes = [...new Set(csv.map(d => d.episode))]
+        var container = document.getElementById("accordionGroup");
+        container.innerHTML = "";
         for (var i = 0; i < titles.length; i++) {
-            title = titles[i]
-            episode = episodes[i]
-            num = (i + 1).toString()
-            addAccordion(container = container, num = num, title = title, episode = episode)
-        }
-    })
-}
-
-function getActiveButtons() {
-    var buttons = document.querySelectorAll(".btn-primary");
-    var actives = document.querySelectorAll(".btn-primary.active");
-    var all = [];
-    var selected = [];
-    buttons.forEach(button => all.push(button.innerHTML));
-    actives.forEach(button => selected.push(button.innerHTML));
-    console.log(selected);
-    document.getElementById('values').innerHTML = "Selected words: " + selected.join(", ").toString();
-    all.forEach(function (value) {
-        var group = document.getElementById(value);
-        circle = group.childNodes[0]
-        text = group.childNodes[1]
-        if (selected.includes(value)) {
-            circle.setAttribute("fill", "red")
-            text.style.fill = "red"
-        } else {
-            circle.setAttribute("fill", "black")
-            text.style.fill = "black"
-        }
-    })
-    d3.csv(csv_url, function (csv) {
-        titles = [...new Set(csv.map(d => d.title))];
-        episodes = [...new Set(csv.map(d => d.episode))];
-        var accordions = document.querySelectorAll(".accordion-item")
-        accordions.forEach(function (d) {
-            var title = d.getElementsByClassName('accordion-button')[0].innerHTML
-            var episode = d.getElementsByClassName('accordion-body')[0].innerHTML
-            var string = (title + " " + episode).toLowerCase()
-            if (selected.every(v => string.includes(v))) {
-                d.hidden = false
-            } else {
-                d.hidden = true
-            }
-        });
+            title = titles[i];
+            episode = episodes[i];
+            num = (i + 1).toString();
+            addAccordion(container = container, num = num, title = title, episode = episode);
+        };
     });
-}
+};
+function getActiveButtons() {
+    var buttons = document.querySelectorAll(".btn-primary")
+    var actives = document.querySelectorAll(".btn-primary.active")
+    var all = []
+    var selected = []
+    buttons.forEach(button => all.push(button.innerHTML))
+    actives.forEach(button => selected.push(button.innerHTML))
+    console.log(selected)
+    document.getElementById('values').innerHTML = "Selected words: " + selected.join(", ").toString()
+    all.forEach(function (value) {
+        var group = document.getElementById(value)
+        circle = group.childNodes[0];
+        text = group.childNodes[1];
+        if (selected.includes(value)) {
+            circle.setAttribute("fill", "red");
+            text.style.fill = "red";
+        } else {
+            circle.setAttribute("fill", "black");
+            text.style.fill = "black";
+        };
+    });
+    d3.csv(csv_url, function (csv) {
+        titles = [...new Set(csv.map(d => d.title))]
+        episodes = [...new Set(csv.map(d => d.episode))]
+        var accordions = document.querySelectorAll(".accordion-item");
+        accordions.forEach(function (d) {
+            var title = d.getElementsByClassName('accordion-button')[0].innerHTML;
+            var episode = d.getElementsByClassName('accordion-body')[0].innerHTML;
+            var string = (title + " " + episode).toLowerCase();
+            if (selected.every(v => string.includes(v))) {
+                d.hidden = false;
+            } else {
+                d.hidden = true;
+            };
+        });
+    })
+};
 
 function renderSVG() {
     var width = 500,
@@ -157,16 +150,16 @@ function refreshAccordion() {
     });
 }
 
-function resetWords(){
+function resetWords() {
     console.log('reset button pressed')
     var actives = document.querySelectorAll(".btn-primary.active");
-    
-    actives.forEach(function(d){
+
+    actives.forEach(function (d) {
         console.log(d.className)
         d.className = d.className.replace("active", "")
     });
     getActiveButtons()
-}
+};
 
 renderButtons();
 renderSVG();
@@ -174,7 +167,6 @@ renderStories();
 getActiveButtons();
 
 $(document).ready(function () {
-
     $(".btn-primary").on('click', function () {
         console.log("clicked");
         getActiveButtons();
